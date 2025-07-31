68440d14d7a5f243f663b25c22aee0530c6e44fc
Nakaba NAGANO <nakabanagano@NakabanoMacBook-Pro.local>
2025-07-31 13:59:46 +0900

    Fix server.js

--- server.js
+++ server.js
@@ -192,7 +192,7 @@ app.get('/api/residents', async (req, res) => {
     const [results] = await pool.query(`
       SELECT 
         r.*, 
-        g.name AS group_home_name,
+        g.group_home_name,
         g.unit_name
       FROM residents r
       LEFT JOIN group_homes g ON r.group_home_id = g.id
