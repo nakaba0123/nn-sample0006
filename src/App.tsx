3820c1617f5b047ee2f1287be026bece856e458d
Nakaba NAGANO <nakaba.nagano@gmail.com>
2025-08-03 12:42:31 +0900

    Fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -402,8 +402,8 @@ useEffect(() => {
 
       setRawResidents(residentsRes.data);
       setDisabilityHistories(historiesRes.data); // ← ??ここが必要！
-      setGroupHomes(groupHomesRes.data); // ← 追加！
-//      setExpansionRecords(expansionsRes.data); // ← 追加！
+//      setGroupHomes(groupHomesRes.data); // ← 追加！
+      setExpansionRecords(expansionsRes.data); // ← 追加！
 
 
     } catch (err) {
