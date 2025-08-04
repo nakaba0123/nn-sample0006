613daca88bccb854894d9cc0b9e9ef44ffd2b4c6
Nakaba NAGANO <nakabanagano@NakabanoMacBook-Pro.local>
2025-08-04 17:13:15 +0900

    Fix multiplex

--- src/util/mapGroupHome.ts
+++ src/util/mapGroupHome.ts
@@ -1,15 +1,18 @@
 // util/mapGroupHome.ts
-export const mapGroupHome = (gh: any) => ({
+export const mapGroupHome = (gh: any) => {
   console.log("💡 transforming group home:", gh);
-  id: gh.id,
-  propertyName: gh.property_name,
-  unitName: gh.unit_name,
-  postalCode: gh.postal_code,
-  address: gh.address,
-  phoneNumber: gh.phone_number,
-  commonRoom: gh.common_room,
-  residentRooms: gh.resident_rooms,
-  openingDate: gh.opening_date,
-  createdAt: gh.created_at,
-});
+  return {
+    id: gh.id,
+    propertyName: gh.property_name,
+    unitName: gh.unit_name,
+    postalCode: gh.postal_code,
+    address: gh.address,
+    phoneNumber: gh.phone_number,
+    commonRoom: gh.common_room,
+    openingDate: gh.opening_date,
+    createdAt: gh.created_at,
+    residentRooms: gh.resident_rooms,
+    expansions: gh.expansions,
+  };
+};
 
