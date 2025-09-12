701ede3252c8e1bb9dba9590e60df9b736494163
Nakaba NAGANO <nakabanagano@NakabanoMacBook-Pro.local>
2025-09-12 17:38:32 +0900

    Fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -665,8 +665,10 @@ const handleResidentSubmit = async (resident: Resident) => {
         const [residentsRes,
                historiesRes,
         ] = await Promise.all([
-          axios.get("/api/residents"),
-          axios.get("/api/disability_histories")
+//          axios.get("/api/residents"),
+//          axios.get("/api/disability_histories")
+          fetchWithRetry("/api/residents"),
+          fetchWithRetry("/api/disability_histories"),
         ]);
 
         console.log("? residents fetched:", residentsRes.data);
