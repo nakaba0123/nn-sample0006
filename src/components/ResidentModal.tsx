4f681d9aaf565bfc9978044aefe2600525afaf4e
Nakaba NAGANO <nakabanagano@NakabanoMacBook-Pro.local>
2025-10-17 11:39:50 +0900

    Fix server.js

--- src/components/ResidentModal.tsx
+++ src/components/ResidentModal.tsx
@@ -782,7 +782,7 @@ console.log("formData: ", formData);
   <option value="">物件・ユニットを選択 *</option>
   {allUnits().map((unit) => (
     <option key={unit.id} value={unit.id}>
-      {unit.propertyName}／{unit.unitName}
+      {unit.facilityCode}：{unit.propertyName}／{unit.unitName}
     </option>
   ))}
 </select>
